d78f4451ca5baedf379fa7db598980ed6bb25194
Yariel <cubanboy4life@gmail.com>
2018-06-22 22:35:51 +0000

    2nd commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -21,7 +21,7 @@ export class Home extends React.Component{
     }
     
     handleFormSubmit(e) {
-      console.log("Creating task with title: ",this.state.taskInput);
+    //console.log("Creating task with title: ",this.state.taskInput);
       e.preventDefault();
       this.setState({
         todos: this.state.todos.concat([{
@@ -41,40 +41,41 @@ export class Home extends React.Component{
 
   render() {
     var tasksToRender = this.state.todos.map((task) => {
-	       return (<li key={task.id}>
-    <div className="view">
-        <label>{task.title}</label>
-        <button className="destroy" onClick={() => this.deleteTask(task.id)}></button>
-    </div>
-</li>);
-	        });
     return (
-        <section className="todoapp">
-            <header className="header">
-                <h1>todos</h1>
-                <form onSubmit={this.handleFormSubmit.bind(this)}>
-                    <input
-              autoFocus={true}
-              className="new-todo"
-              placeholder="What needs to be done?"
-              value={this.state.taskInput}
-              onChange={(evt) => this.setState({ taskInput: evt.target.value}) }
-            />
-                </form>
-            </header>
-            <section className="main">
-                <ul className="todo-list">
-                    {tasksToRender}
-                </ul>
-            </section>
-            <footer className="footer">
-                <span className="todo-count">
-                    <strong>
-                        {this.state.todos.filter(key => !key.done).length}
-                    </strong> item left
-                </span>
-            </footer>
-        </section>
+        <li key={task.id}>
+            <div className="view">
+                <label>{task.title}</label>
+                <button className="destroy" onClick={() => this.deleteTask(task.id)}></button>
+            </div>
+        </li>);
+    });
+                return (
+                    <section className="todoapp">
+                        <header className="header">
+                            <h1>todos</h1>
+                            <form onSubmit={this.handleFormSubmit.bind(this)}>
+                                <input
+                            autoFocus={true}
+                          className="new-todo"
+                          placeholder="What needs to be done?"
+                          value={this.state.taskInput}
+                          onChange={(evt) => this.setState({ taskInput: evt.target.value}) }
+                />
+                            </form>
+                        </header>
+                        <section className="main">
+                            <ul className="todo-list">
+                                {tasksToRender}
+                            </ul>
+                        </section>
+                        <footer className="footer">
+                            <span className="todo-count">
+                                <strong>
+                                    {this.state.todos.filter(key => !key.done).length}
+                                </strong> item left
+                            </span>
+                        </footer>
+                    </section>
     );
   }
 }
